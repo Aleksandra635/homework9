118caed5291d35e4a40344f86334ad771ff559c5
Aleksandrs <shalunovaaa@yandex.ru>
2023-07-19 17:38:18 +0300

    Task2

---
+++ task2/Program.cs
@@ -0,0 +1,19 @@
+﻿int a = Prompt("Введите минимальное число: ");
+int b = Prompt("Введите максимальное число: ");
+Console.WriteLine($"{Recursy(b, a)}");
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int Recursy (int b, int a)
+{
+    if (b < a)
+    {
+    return 0;
+    }
+    return Recursy(b-1, a) + b;
+}
